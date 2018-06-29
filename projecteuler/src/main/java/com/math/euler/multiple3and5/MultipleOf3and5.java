68af3ed6a1c2834e39ff0895332df3462aa5e367
ravindra <nalavde@gmail.com>
2018-06-29 18:42:30 +0530

    Added Source configuration for java 8 and Added implementation for multiplication of 3 & 5

--- projecteuler/src/main/java/com/math/euler/multiple3and5/MultipleOf3and5.java
+++ projecteuler/src/main/java/com/math/euler/multiple3and5/MultipleOf3and5.java
@@ -1,12 +1,13 @@
 package com.math.euler.multiple3and5;
 
+import java.util.stream.IntStream;
+
 public class MultipleOf3and5 {
 
 	public static void main(String[] args) {
-		
-		
-
-		
+		IntStream infiniteStream = IntStream.iterate(0, i -> i + 1);
+		int sum = infiniteStream.limit(1000).filter(e -> (e % 3 == 0 || e % 5 == 0 )).sum();
+		System.out.println(sum);
 	}
 
 }
